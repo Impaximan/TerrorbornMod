cb68590f3c7eb13612e0731b70e35c426d5a88bb
Justin Wallace <penumbralgamingyt@gmail.com>
2021-11-14 17:52:25 -0500

    Incendiary Stalker moment

--- NPCs/Incendiary/IncendiaryStalker.cs
+++ NPCs/Incendiary/IncendiaryStalker.cs
@@ -7,7 +7,7 @@ using Terraria.ModLoader;
 
 namespace TerrorbornMod.NPCs.Incendiary
 {
-    class IncendiaryProbe : ModNPC
+    class IncendiaryStalker : ModNPC
     {
         public override void SetStaticDefaults()
         {
@@ -19,11 +19,11 @@ namespace TerrorbornMod.NPCs.Incendiary
             npc.noGravity = true;
             npc.noTileCollide = true;
             npc.aiStyle = -1;
-            npc.width = 40;
-            npc.height = 28;
+            npc.width = 54;
+            npc.height = 38;
             npc.damage = 45;
             npc.defense = 20;
-            npc.lifeMax = 425;
+            npc.lifeMax = 1200;
             npc.HitSound = SoundID.NPCHit4;
             npc.DeathSound = SoundID.NPCDeath14;
             npc.value = 250;
@@ -57,7 +57,7 @@ namespace TerrorbornMod.NPCs.Incendiary
 
         public override float SpawnChance(NPCSpawnInfo spawnInfo)
         {
-            if (TerrorbornPlayer.modPlayer(spawnInfo.player).ZoneIncendiary && !NPC.downedGolemBoss)
+            if (TerrorbornPlayer.modPlayer(spawnInfo.player).ZoneIncendiary && NPC.downedGolemBoss)
             {
                 return SpawnCondition.Sky.Chance * 0.06f;
             }
@@ -85,17 +85,20 @@ namespace TerrorbornMod.NPCs.Incendiary
                 projectileWait--;
                 if (projectileWait <= 0)
                 {
-                    projectileWait = 30 + (int)(30f / ((float)npc.lifeMax / (float)npc.life));
-                    float speed = 15f;
+                    projectileWait = 20 + (int)(20f / ((float)npc.lifeMax / (float)npc.life));
+                    float speed = 18f;
                     Vector2 velocity = npc.DirectionTo(player.Center + player.velocity * (npc.Distance(player.Center) / speed)) * speed;
                     npc.velocity += npc.DirectionFrom(player.Center) * 3;
                     Main.PlaySound(SoundID.Item33, npc.Center);
+                    float offset = MathHelper.ToRadians(30);
                     Projectile.NewProjectile(npc.Center, velocity, ModContent.ProjectileType<IncendiaryProbeLaser>(), 90 / 4, 0);
+                    Projectile.NewProjectile(npc.Center, velocity.RotatedBy(offset), ModContent.ProjectileType<IncendiaryProbeLaser>(), 120 / 4, 0);
+                    Projectile.NewProjectile(npc.Center, velocity.RotatedBy(-offset), ModContent.ProjectileType<IncendiaryProbeLaser>(), 120 / 4, 0);
                 }
             }
             else
             {
-                float moveSpeed = 0.2f;
+                float moveSpeed = 0.4f;
                 npc.velocity += npc.DirectionTo(player.Center) * moveSpeed;
             }
 
@@ -111,34 +114,6 @@ namespace TerrorbornMod.NPCs.Incendiary
             npc.rotation = MathHelper.ToRadians(npc.velocity.X * 1.5f);
 
             npc.velocity *= 0.96f;
-        }
-    }
-
-    class IncendiaryProbeLaser : ModProjectile
-    {
-        public override void SetDefaults()
-        {
-            projectile.width = 22;
-            projectile.height = 6;
-            projectile.friendly = false;
-            projectile.hostile = true;
-            projectile.tileCollide = false;
-            projectile.ignoreWater = false;
-            projectile.penetrate = 1;
-            projectile.timeLeft = 600;
-        }
-
-        public override void OnHitPlayer(Player target, int damage, bool crit)
-        {
-            target.AddBuff(BuffID.OnFire, 60 * 5);
-        }
-
-        public override void AI()
-        {
-            projectile.rotation = projectile.velocity.ToRotation();
-            Dust dust = Dust.NewDustPerfect(projectile.Center, DustID.Fire, Vector2.Zero);
-            dust.noGravity = true;
-
         }
     }
 }
