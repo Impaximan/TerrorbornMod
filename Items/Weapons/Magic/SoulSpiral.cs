6e45bab573da9294cb2893fd6434e937ecf2a058
Justin Wallace <penumbralgamingyt@gmail.com>
2021-12-28 14:18:02 -0500

    Remade the soul spiral's visuals

--- Items/Weapons/Magic/SoulSpiral.cs
+++ Items/Weapons/Magic/SoulSpiral.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System.Collections.Generic;
 
 namespace TerrorbornMod.Items.Weapons.Magic
 {
@@ -64,6 +65,44 @@ namespace TerrorbornMod.Items.Weapons.Magic
     {
         public override string Texture { get { return "Terraria/Projectile_" + ProjectileID.EmeraldBolt; } }
 
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[this.projectile.type] = 10;
+            ProjectileID.Sets.TrailingMode[this.projectile.type] = 1;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, null, null, null, null, Main.GameViewMatrix.ZoomMatrix);
+
+            BezierCurve bezier = new BezierCurve();
+            bezier.Controls.Clear();
+            foreach (Vector2 pos in projectile.oldPos)
+            {
+                if (pos != Vector2.Zero && pos != null)
+                {
+                    bezier.Controls.Add(pos);
+                }
+            }
+
+            if (bezier.Controls.Count > 1)
+            {
+                List<Vector2> positions = bezier.GetPoints(50);
+                for (int i = 0; i < positions.Count; i++)
+                {
+                    float mult = (float)(positions.Count - i) / (float)positions.Count;
+                    Vector2 drawPos = positions[i] - Main.screenPosition + projectile.Size / 2;
+                    Color color = projectile.GetAlpha(Color.Lerp(Color.MediumPurple, Color.LightPink, mult)) * mult;
+                    TBUtils.Graphics.DrawGlow_1(spriteBatch, drawPos, (int)(25f * mult), color);
+                }
+            }
+
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, Main.GameViewMatrix.ZoomMatrix);
+            return false;
+        }
+
         public override void SetDefaults()
         {
             projectile.width = 16;
@@ -75,7 +114,7 @@ namespace TerrorbornMod.Items.Weapons.Magic
             projectile.hostile = false;
             projectile.magic = true;
             projectile.ignoreWater = true;
-            projectile.hide = true;
+            projectile.hide = false;
             projectile.timeLeft = (int)(360 / rotationSpeed);
             projectile.usesIDStaticNPCImmunity = true;
             projectile.idStaticNPCHitCooldown = 10;
